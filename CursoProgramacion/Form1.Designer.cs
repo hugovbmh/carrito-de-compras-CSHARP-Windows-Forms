885cfbfb445cdbce2a190b29b0f8832aef1d57a4
Hugo <hugovilchezbeltran@gmail.com>
2022-11-24 11:13:02 -0500

    Carrito de compras Windows Forms

---
+++ CursoProgramacion/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CursoProgramacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.listaProductos = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.cboProductos = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigoDescuento = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(366, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hola Mundo";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(435, 122);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 53);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Agregar al listado";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listaProductos
+            // 
+            this.listaProductos.FormattingEnabled = true;
+            this.listaProductos.ItemHeight = 20;
+            this.listaProductos.Location = new System.Drawing.Point(61, 63);
+            this.listaProductos.Name = "listaProductos";
+            this.listaProductos.Size = new System.Drawing.Size(326, 184);
+            this.listaProductos.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(550, 122);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(168, 53);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular Precio";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // cboProductos
+            // 
+            this.cboProductos.FormattingEnabled = true;
+            this.cboProductos.Location = new System.Drawing.Point(435, 63);
+            this.cboProductos.Name = "cboProductos";
+            this.cboProductos.Size = new System.Drawing.Size(237, 28);
+            this.cboProductos.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(435, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Código de Descuento";
+            // 
+            // txtCodigoDescuento
+            // 
+            this.txtCodigoDescuento.Location = new System.Drawing.Point(610, 215);
+            this.txtCodigoDescuento.Name = "txtCodigoDescuento";
+            this.txtCodigoDescuento.Size = new System.Drawing.Size(125, 27);
+            this.txtCodigoDescuento.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 537);
+            this.Controls.Add(this.txtCodigoDescuento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboProductos);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.listaProductos);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ListBox listaProductos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox cboProductos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigoDescuento;
+    }
+}
+
---
+++ CursoProgramacion/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoProgramacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string productoSeleccionado = cboProductos.SelectedItem.ToString();
+            listaProductos.Items.Add(productoSeleccionado);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboProductos.Items.Add("Pantalon");
+            cboProductos.Items.Add("Camisa");
+            cboProductos.Items.Add("Zapatos");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double precioFinal = 0;
+            var totalProductosCargados = listaProductos.Items.Count;
+            for (int i = 0; i < totalProductosCargados; i++)
+            {
+                string productoElegido = listaProductos.Items[i].ToString();
+                if (productoElegido == "Pantalon")
+                {
+                    precioFinal += 20;
+                }
+                else if (productoElegido == "Camisa")
+                {
+                    precioFinal += 35;
+                }
+                else if (productoElegido == "Zapatos")
+                {
+                    precioFinal += 24;
+                }
+            }
+            if (txtCodigoDescuento.Text == "NAVIDAD")
+            {
+                precioFinal = precioFinal * 0.90;
+            }
+            MessageBox.Show("El precio final es: $" + precioFinal);
+        }
+    }
+}
